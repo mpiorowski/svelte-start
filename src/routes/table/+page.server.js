5e260ec7a6ba1e27461dd53708b4699fa1bbe7d9
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-09-16 05:54:48 +0200

    update

--- src/routes/table/+page.server.js
+++ src/routes/table/+page.server.js
@@ -1,22 +1,21 @@
 import { z } from "zod";
 import { fail } from "@sveltejs/kit";
-import { faker } from "@faker-js/faker";
 import { pagination } from "$lib/overlay/pagination";
 
-const data = Array(24)
-    .fill(0)
-    .map(() => ({
-        name: faker.person.fullName(),
-        title: faker.person.jobTitle(),
-        email: faker.internet.email(),
-        role: ["Member", "Admin"][Math.floor(Math.random() * 2)],
-    }));
+//const data = Array(24)
+//    .fill(0)
+//    .map(() => ({
+//        name: faker.person.fullName(),
+//        title: faker.person.jobTitle(),
+//        email: faker.internet.email(),
+//        role: ["Member", "Admin"][Math.floor(Math.random() * 2)],
+//    }));
 
 /** @type {import('./$types').PageServerLoad} */
 export function load({ url }) {
     let p = Number(url.searchParams.get("p")) || 1;
     return {
-        pagination: pagination([...data, ...data, ...data], p),
+        pagination: pagination([], p),
     };
 }
 
