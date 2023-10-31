2e200cba9995a983b23311aa1e13d77a3e0c7459
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-10-31 19:32:43 +0100

    update

--- src/lib/form/Button.svelte
+++ src/lib/form/Button.svelte
@@ -9,6 +9,8 @@
     export let formaction = undefined;
     /** @type {string} */
     export let href = "";
+    /** @type {boolean} */
+    export let loading = false;
 
     let className = "";
     export { className as class };
@@ -20,6 +22,7 @@
         {formaction}
         {form}
         {type}
+        disabled={loading}
         class="group inline-flex w-full items-center justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
         {variant === 'primary' &&
             'bg-indigo-600 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-indigo-600'}
@@ -32,6 +35,30 @@
         {className}
         "
     >
+        {#if loading}
+            <svg
+                class="h-5 w-5 animate-spin text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+            >
+                <circle
+                    class="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    stroke-width="4"
+                />
+                <path
+                    class="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+            </svg>
+        {:else}
+            <slot name="icon" />
+        {/if}
         <slot />
     </button>
 {/if}
