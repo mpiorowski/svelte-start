8ae25d5bae766cacb3be75366bb4574c9d86caf8
mateuszpiorowski <mateuszpiorowski@gmail.com>
2023-12-12 12:31:02 +0100

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,8 @@
     import Button from "$lib/form/Button.svelte";
     import MenuIcon from "$lib/icons/MenuIcon.svelte";
     import Drawer from "$lib/ui/Drawer.svelte";
-    import Nav from "./Nav.svelte";
     import Avatar from "$lib/ui/Avatar.svelte";
+    import { page } from "$app/stores";
 
     let open = false;
     const navs = [
@@ -20,7 +20,18 @@
         <Drawer position="left" title="Svelte Start" bind:open>
             <nav class="flex flex-col space-y-4">
                 {#each navs as nav}
-                    <Nav label={nav.label} href={nav.href} />
+                    <a
+                        href={nav.href}
+                        class="peer rounded-md px-3 py-2 text-sm font-medium focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2
+                {$page.url.pathname === nav.href
+                            ? 'bg-indigo-100 text-indigo-700'
+                            : 'text-gray-500 hover:text-gray-700'}"
+                        aria-current={$page.url.pathname === nav.href
+                            ? "page"
+                            : undefined}
+                    >
+                        {nav.label}
+                    </a>
                 {/each}
             </nav>
         </Drawer>
@@ -38,7 +49,18 @@
         <div class="hidden sm:block">
             <nav class="flex gap-x-4" aria-label="Tabs">
                 {#each navs as nav}
-                    <Nav label={nav.label} href={nav.href} />
+                    <a
+                        href={nav.href}
+                        class="peer rounded-md px-3 py-2 text-sm font-medium focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2
+                {$page.url.pathname === nav.href
+                            ? 'bg-indigo-100 text-indigo-700'
+                            : 'text-gray-500 hover:text-gray-700'}"
+                        aria-current={$page.url.pathname === nav.href
+                            ? "page"
+                            : undefined}
+                    >
+                        {nav.label}
+                    </a>
                 {/each}
                 <div class="ml-auto flex items-center justify-center">
                     <Avatar />
